372b050b6d5a6e5f6bccdc56be990c3d1572398e
MauroJuliano <43898163+MauroJuliano@users.noreply.github.com>
2021-07-19 18:31:31 -0400

    Initial Commit

---
+++ WaveFormLoading/WaveFormLoading.h
@@ -0,0 +1,18 @@
+//
+//  WaveFormLoading.h
+//  WaveFormLoading
+//
+//  Created by user195713 on 7/19/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for WaveFormLoading.
+FOUNDATION_EXPORT double WaveFormLoadingVersionNumber;
+
+//! Project version string for WaveFormLoading.
+FOUNDATION_EXPORT const unsigned char WaveFormLoadingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WaveFormLoading/PublicHeader.h>
+
+
